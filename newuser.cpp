b70523de1c1054e26695a6a3869cbce6a1daf5c5
Rohanrockyy <rohanrocky001@gmail.com>
2022-04-25 16:03:38 +0530

    Userlogin

--- newuser.cpp
+++ newuser.cpp
@@ -4,16 +4,18 @@
 #include<stdlib.h>
 #include<string.h>
 using namespace std;
-void login();
 void registr();
-
+void login();
 int main()
 {
         int choice;
-        cout<<"                      Welcome to login page                               \n\n";
-        cout<<"*******        MENU        ***********\n\n";
+        cout<<" Welcome to login page \n";
+	cout<<endl;
+        cout<<" MENU \n";
+	cout<<endl;
         cout<<"1.REGISTER"<<endl;
-        cout<<"2.Exit"<<endl;
+	cout<<"2.Login"<<endl;
+        cout<<"3.Exit"<<endl;
         cout<<"\nEnter your choice :";
         cin>>choice;
         cout<<endl;
@@ -22,22 +24,60 @@ int main()
                 case 1:
                         registr();
                         break;
-                case 2:
+		case 2:
+			login();
+			break;
+                case 3:
                         cout<<"You have exited successfully\n\n";
                         break;
                 default:
-                        system("cls");
-                        cout<<"You've made a mistake , give it a try again\n"<<endl;
+			system("clear");
+                        cout<<"Invalid choice! Try again\n"<<endl;
                         main();
         }
        
+}
+void login()
+{
+        int a;
+        string user,pass,u,p;
+        cout<<"please enter the following details"<<endl;
+        cout<<"USERNAME :";
+        cin>>user;
+        cout<<"PASSWORD :";
+        cin>>pass;
+        
+        ifstream input("userdetails.txt");
+        while(input>>u>>p)
+        {
+                if(u==user && p==pass)
+        
+                {
+                        a=1;
+                }
+        }
+        input.close();
+        if(a==1)
+        {
+                cout<<"\nHello "<<user<<"\nYou are logged in successfully\n";
+		cout<<endl;
+                cin.get();
+		cin.get();
+                main();
+        }
+        else
+        {	
+		system("clear");
+                cout<<"\nLOGIN ERROR\nPlease check your username and password\n";
+		cout<<endl;
+                main();
+        }
 }
 
 void registr()
 {
        
         string reguser,regpass,confirmpass,regmobile,regemail,rpass,cpass;
-        system("cls");
         cout<<"Enter the username :";
         cin>>reguser;
         cout<<"\nEnter the password :";
@@ -60,19 +100,15 @@ void registr()
 		cpass=confirmpass;
 
         }
-	
-	
-
-        cout<<"\nEnter the mobile no :";
+	cout<<"\nEnter the mobile no :";
         cin>>regmobile;
         cout<<"\nEnter the email :";
         cin>>regemail;
        
         ofstream data("userdetails.txt",ios::app);
         data<<reguser<<' '<<regpass<<' '<<regmobile<<' '<<regemail<<' '<<endl;
-        system("cls");
+	system("clear");
         cout<<"\nRegistration Sucessful\n";
-        main();
-       
-       
+	cout<<endl;
+        main();       
 }
